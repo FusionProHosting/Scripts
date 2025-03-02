79a0730ecc28aaea17972e1e6c0e5080f18ae0da
Tykari <tykariw@hotmail.com>
2025-03-02 00:47:31 -0600

    Update installwings.sh

--- Pterodactyl/installwings.sh
+++ Pterodactyl/installwings.sh
@@ -4,3 +4,5 @@ mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
 apt install -y certbot
+cd /etc/systemd/system
+wget https://github.com/FusionProHosting/Scripts/blob/master/Pterodactyl/wings.service
